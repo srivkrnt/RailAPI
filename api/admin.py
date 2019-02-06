e9ac4c381cc720f6d5b9c16f4581f692731bfa6d
Vikrant Srivastava <sri.vkrnt@gmail.com>
2019-02-06 23:53:31 +0530

    Registering PnrDetails to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TrainDetails
+from .models import TrainDetails, PnrDetails
 
 admin.site.register(TrainDetails)
+admin.site.register(PnrDetails)
