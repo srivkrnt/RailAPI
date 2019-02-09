8ee2c1e5b1b0d99ae963ba0e74f43fb8e5e910e2
Vineet kumar singh <singhvineet486@gmail.com>
2019-02-09 14:17:29 +0530

    Updating services

--- api/services.py
+++ api/services.py
@@ -8,9 +8,13 @@ def getTrainDetails(train_no):
     page = requests.get(url)
     soup = BeautifulSoup(page.text,'html.parser')
     details = []
+    for name in soup.find_all("div",class_="col-md-12 heading-wrap"):
+        name.has_attr("h1")
+        details.append(name.text.split('Route and Schedule')[0].strip('\n'))
+    
     for train_details in soup.find_all("th",class_="station-name"):
         details.append(train_details.text)
-    return [details[0], details[-1]]
+    return [details[0],details[1], details[-1]]
 
 def getPnrDetails(pnr):
     url='https://www.railyatri.in/pnr-status/' + str(pnr)
